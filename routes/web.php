b6f7c0fd6f0f99be68e0979d12cd157af402c3c1
azzorraz <azzorraz@gmail.com>
2026-06-24 13:33:33 +0700

    awal

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    '/' => [
+        'controller' => 'HomeController',
+        'method' => 'index'
+    ]
+];
---
+++ views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>GTK Form</title>
+</head>
+<body>
+
+<h1>Hello GTK Form 🚀</h1>
+
+</body>
+</html>
